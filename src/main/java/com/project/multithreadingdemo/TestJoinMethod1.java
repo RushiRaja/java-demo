c7e0c1d5fe43fad0a092d552b34065966707d3ae
Rushiraja Panuganti <p.rushiraja@gmail.com>
2019-01-23 15:43:41 -0600

    join() method examples

---
+++ src/main/java/com/project/multithreadingdemo/TestJoinMethod1.java
@@ -0,0 +1,43 @@
+package com.project.multithreadingdemo;
+
+/**
+ * The join() method waits for a thread to die. In other words, it causes the
+ * currently running threads to stop executing until the thread it joins with
+ * completes its task.
+ * 
+ * 
+ * 
+ * As you can see in the example,when t1 completes its task then t2 and t3
+ * starts executing.
+ * 
+ * @author Rushiraja
+ *
+ */
+public class TestJoinMethod1 extends Thread {
+	public void run() {
+		for (int i = 1; i <= 5; i++) {
+			try {
+				Thread.sleep(500);
+			} catch (Exception e) {
+				System.out.println(e);
+			}
+			System.out.println(i);
+		}
+	}
+
+	public static void main(String args[]) {
+		TestJoinMethod1 t1 = new TestJoinMethod1();
+		TestJoinMethod1 t2 = new TestJoinMethod1();
+		TestJoinMethod1 t3 = new TestJoinMethod1();
+		t1.start();
+		try {
+			t1.join();
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+
+		t2.start();
+		t3.start();
+
+	}
+}
